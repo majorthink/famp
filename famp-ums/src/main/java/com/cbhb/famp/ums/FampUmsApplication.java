2a8ff7159834543ca6735f9f4f54aad93af9c73b
majorthink <179580626@qq.com>
2020-02-26 22:19:13 +0800

    电子台账平台

---
+++ famp-ums/src/main/java/com/cbhb/famp/ums/FampUmsApplication.java
@@ -0,0 +1,13 @@
+package com.cbhb.famp.ums;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FampUmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FampUmsApplication.class, args);
+	}
+
+}
